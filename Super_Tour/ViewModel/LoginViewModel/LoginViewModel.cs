2e5a7a4c6acc4a99228d546d9dfad9513dc0afbc
tuan <thanhtuanse16@gmail.com>
2023-04-24 18:16:12 +0700

    add menu

--- Super_Tour/ViewModel/LoginViewModel/LoginViewModel.cs
+++ Super_Tour/ViewModel/LoginViewModel/LoginViewModel.cs
@@ -35,9 +35,15 @@ namespace Super_Tour.ViewModel.LoginViewModel
 
         public LoginViewModel()
         {
-            CommandLogin = new RelayCommand(Login);
+            CommandLogin = new RelayCommand(Login,CanLogin);
             CommandForgotPassword = new RelayCommand(MoveToForgotPass);
         }
+
+        private bool CanLogin(object obj)
+        {
+            if()
+        }
+
         public void MoveToForgotPass(object a)
         {
             ForgotPass_EmailView view = new ForgotPass_EmailView();
