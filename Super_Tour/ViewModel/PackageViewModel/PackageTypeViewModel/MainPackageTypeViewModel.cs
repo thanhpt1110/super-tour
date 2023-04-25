34cd96e36c04ed70c7136acd2919fabdbdf16bd9
tuan <thanhtuanse16@gmail.com>
2023-04-25 17:16:12 +0700

    add DataGrid for PackageType

--- Super_Tour/ViewModel/PackageViewModel/PackageTypeViewModel/MainPackageTypeViewModel.cs
+++ Super_Tour/ViewModel/PackageViewModel/PackageTypeViewModel/MainPackageTypeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,42 @@ using Super_Tour.Ultis;
 
 namespace Super_Tour.ViewModel
 {
-    internal class MainPackageTypeViewModel: ObservableObject
+    internal class MainPackageTypeViewModel : ObservableObject
     {
+        public class PackageTypeTest{
+            private string _packageTypeID;
+            private string _packageTypeName;
+            private string _description;
+
+            public string PackageTypeID { get => _packageTypeID; set => _packageTypeID = value; }
+            public string PackageTypeName { get => _packageTypeName; set => _packageTypeName = value; }
+            public string Description { get => _description; set => _description = value; }
+            public PackageTypeTest() { 
+            }
+            public PackageTypeTest(string packageTypeID, string packageTypeName, string description)
+            {
+                this.PackageTypeID = packageTypeID;
+                this.PackageTypeName = packageTypeName;
+                this.Description = description;
+            }
+        }
+        private ObservableCollection<PackageTypeTest> _packageTypeTests;
+
+        public ObservableCollection<PackageTypeTest> PackageTypeTests 
+        { 
+            get => _packageTypeTests;
+            set
+            {
+                _packageTypeTests = value;
+                OnPropertyChanged(nameof(PackageTypeTests));
+            } 
+        }
+
+        public MainPackageTypeViewModel() {
+            PackageTypeTests = new ObservableCollection<PackageTypeTest>();
+            PackageTypeTests.Add(new PackageTypeTest("1", "Ăn uống", "Nội dung của gồm tên quán ăn, địa điểm"));
+            PackageTypeTests.Add(new PackageTypeTest("2", "Tắm biển", "Nội dung của gồm tên bãi, thời gian"));
+            PackageTypeTests.Add(new PackageTypeTest("3", "Tham quan", "Nội dung gồm địa điểm tham quan"));
+        }
     }
 }
