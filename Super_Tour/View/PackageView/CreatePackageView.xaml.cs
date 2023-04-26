bdead206e09c30b789f2adef261e0721f8dadbbb
tuan <thanhtuanse16@gmail.com>
2023-04-26 16:26:02 +0700

    update view

--- Super_Tour/View/PackageView/CreatePackageView.xaml.cs
+++ Super_Tour/View/PackageView/CreatePackageView.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Super_Tour.View
@@ -18,7 +17,7 @@ namespace Super_Tour.View
     /// <summary>
     /// Interaction logic for CreatePackageView.xaml
     /// </summary>
-    public partial class CreatePackageView : UserControl
+    public partial class CreatePackageView : Window
     {
         public CreatePackageView()
         {
