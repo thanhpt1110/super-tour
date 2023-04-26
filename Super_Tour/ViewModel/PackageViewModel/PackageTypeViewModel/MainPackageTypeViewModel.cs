9747db487674b3aed26dee3af7f5ced9365cfe48
PNB309 <nguyen.phucbinh445@gmail.com>
2023-04-27 00:27:04 +0700

    Reload new Datachange

--- Super_Tour/ViewModel/PackageViewModel/PackageTypeViewModel/MainPackageTypeViewModel.cs
+++ Super_Tour/ViewModel/PackageViewModel/PackageTypeViewModel/MainPackageTypeViewModel.cs
@@ -11,13 +11,17 @@ using Super_Tour.View;
 using Super_Tour.Model;
 using System.Windows;
 using System.Threading;
+using System.Data.Entity;
+using System.Windows.Threading;
 
 namespace Super_Tour.ViewModel
 {
     internal class MainPackageTypeViewModel : ObservableObject
     {
         private SUPER_TOUR db = new SUPER_TOUR();
+        private List<TYPE_PACKAGE> ListTypePackage;
         private ObservableCollection<TYPE_PACKAGE> _listTypePackages = new ObservableCollection<TYPE_PACKAGE>();
+        private DispatcherTimer timer = new DispatcherTimer();
         public ObservableCollection<TYPE_PACKAGE> ListTypePackages
         {
             get { return _listTypePackages; }
@@ -34,11 +38,32 @@ namespace Super_Tour.ViewModel
 
             OpenCreatePackageTypeViewCommand = new RelayCommand(ExecuteOpenCreatePackageTypeViewCommand);
             DeletePackageInDataGridView = new RelayCommand(ExecuteDeletePackageCommand);
-            /*       Thread thread = new Thread(GetAllPackage);
-                   thread.Start();*/
             LoadAllPackage();
-
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            List<TYPE_PACKAGE> myEntities = db.TYPE_PACKAGEs.ToList();
+            // Kiểm tra dữ liệu có được cập nhật chưa
+            if (!myEntities.SequenceEqual(ListTypePackage))
+            {
+                // Dữ liệu đã được cập nhật
+                // Thực hiện các xử lý cập nhật dữ liệu trong ứng dụng của bạn
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    ListTypePackage = myEntities;
+                    _listTypePackages.Clear();
+                    foreach (TYPE_PACKAGE typePackage in ListTypePackage)
+                    {
+                        ListTypePackages.Add(typePackage);
+                    }
+                });
+            }
+        }
+
         private async Task LoadAllPackage()
         {
             await LoadDataAsync();
@@ -47,7 +72,7 @@ namespace Super_Tour.ViewModel
         {
             await Task.Run(() =>
             {
-                List<TYPE_PACKAGE> ListTypePackage = db.TYPE_PACKAGEs.ToList();
+                 ListTypePackage = db.TYPE_PACKAGEs.ToList();
                 Application.Current.Dispatcher.Invoke(() =>
                 {
                     _listTypePackages.Clear();
@@ -55,9 +80,19 @@ namespace Super_Tour.ViewModel
                     {
                         _listTypePackages.Add(typePackage);
                     }
+                    timer.Start();
                 });
             });
         }
+        private void getAllPackage()
+        {
+            List<TYPE_PACKAGE> ListTypePackage = db.TYPE_PACKAGEs.ToList();
+            _listTypePackages.Clear();
+            foreach (TYPE_PACKAGE typePackage in ListTypePackage)
+            {
+                _listTypePackages.Add(typePackage);
+            }
+        }    
         private void ExecuteDeletePackageCommand(object obj)
         {
             try
@@ -77,7 +112,7 @@ namespace Super_Tour.ViewModel
         {
             CreatePackageTypeView createPackageTypeView = new CreatePackageTypeView();
             createPackageTypeView.ShowDialog();
-            LoadAllPackage();
+            getAllPackage();
         }
     }
 }
