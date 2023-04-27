8a26e758b7157d8d8c53b7dc582b5d4a4c1dcd9a
tuan <thanhtuanse16@gmail.com>
2023-04-28 01:00:17 +0700

    add Tour View

--- Super_Tour/App.xaml.cs
+++ Super_Tour/App.xaml.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace Super_Tour
 {
@@ -14,6 +15,16 @@ namespace Super_Tour
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Lấy hình ảnh biểu tượng và tạo một đối tượng BitmapSource.
+            BitmapSource icon = new BitmapImage(new Uri("pack://application:,,,/Super_Tour;component/Images/Logo.ico"));
+
+            // Đặt hình ảnh biểu tượng cho ứng dụng.
+            Current.MainWindow.Icon = icon;
+        }
         protected void ApplicationStart(object sender, StartupEventArgs e)
         {
             var loginView = new LoginView();
