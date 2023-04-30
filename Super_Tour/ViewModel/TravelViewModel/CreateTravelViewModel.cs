978c046dc441195cd2c561c54f3a943c6e7a1941
tuan <thanhtuanse16@gmail.com>
2023-05-01 02:31:22 +0700

    update CreateTravelView

--- Super_Tour/ViewModel/TravelViewModel/CreateTravelViewModel.cs
+++ Super_Tour/ViewModel/TravelViewModel/CreateTravelViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Student_wpf_application.ViewModels.Command;
+using Super_Tour.Ultis;
+using Super_Tour.View.TravelView;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Super_Tour.ViewModel
 {
-    internal class CreateTravelViewModel
+    internal class CreateTravelViewModel: ObservableObject
     {
+        public CreateTravelViewModel() 
+        {
+            
+        }
+
     }
 }
--- Super_Tour/ViewModel/TravelViewModel/MainTravelViewModel.cs
+++ Super_Tour/ViewModel/TravelViewModel/MainTravelViewModel.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Student_wpf_application.ViewModels.Command;
 using Super_Tour.Ultis;
+using Super_Tour.View.TravelView;
 
 namespace Super_Tour.ViewModel
 {
     internal class MainTravelViewModel: ObservableObject
     {
-
+        public ICommand OpenCreateTravelViewCommand { get; }
+        public MainTravelViewModel() 
+        {
+            OpenCreateTravelViewCommand = new RelayCommand(ExecuteOpenCreateTravelViewCommand);
+        }
+        private void ExecuteOpenCreateTravelViewCommand(object obj)
+        {
+            CreateTravelView createTravelView = new CreateTravelView();
+            createTravelView.ShowDialog();
+        }
     }
 }
