11b92c0d488f03abdb913dbfceb4d2e85111b585
tuan <thanhtuanse16@gmail.com>
2023-05-01 16:52:55 +0700

    add UpdateTravelView

--- Super_Tour/View/TravelView/UpdateTravelView.xaml.cs
+++ Super_Tour/View/TravelView/UpdateTravelView.xaml.cs
@@ -10,15 +10,14 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Super_Tour.View
+namespace Super_Tour.View.TravelView
 {
     /// <summary>
     /// Interaction logic for UpdateTravelView.xaml
     /// </summary>
-    public partial class UpdateTravelView : UserControl
+    public partial class UpdateTravelView : Window
     {
         public UpdateTravelView()
         {
