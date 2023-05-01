29283e77030f90827375da0cf1359aba71e6b4bf
tuan <thanhtuanse16@gmail.com>
2023-05-01 19:25:07 +0700

    remove Operations Header Column icon

--- Super_Tour/View/CustomerView/CreateCustomerView.xaml.cs
+++ Super_Tour/View/CustomerView/CreateCustomerView.xaml.cs
@@ -10,15 +10,14 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Super_Tour.View
+namespace Super_Tour.View.CustomerView
 {
     /// <summary>
     /// Interaction logic for CreateCustomerView.xaml
     /// </summary>
-    public partial class CreateCustomerView : UserControl
+    public partial class CreateCustomerView : Window
     {
         public CreateCustomerView()
         {
--- Super_Tour/View/TravelView/UpdateTravelView.xaml.cs
+++ Super_Tour/View/TravelView/UpdateTravelView.xaml.cs
@@ -23,5 +23,35 @@ namespace Super_Tour.View.TravelView
         {
             InitializeComponent();
         }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
     }
 }
