8fdffbf41bfd6a51de53522e1ac756bee75aa2e0
tuan <thanhtuanse16@gmail.com>
2023-05-02 18:58:26 +0700

    add Account View

--- Super_Tour/ViewModel/AccountViewModel/MainAccountViewModel.cs
+++ Super_Tour/ViewModel/AccountViewModel/MainAccountViewModel.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Student_wpf_application.ViewModels.Command;
 using Super_Tour.Ultis;
+using Super_Tour.View;
 
 namespace Super_Tour.ViewModel
 {
     internal class MainAccountViewModel: ObservableObject
     {
+        public ICommand OpenCreateAccountViewCommand { get; }
+        public MainAccountViewModel() 
+        {
+            OpenCreateAccountViewCommand = new RelayCommand(ExecuteOpenCreateAccountViewCommand);
+        }
 
+        private void ExecuteOpenCreateAccountViewCommand(object obj)
+        {
+            CreateAccountView createAccountView = new CreateAccountView();
+            createAccountView.ShowDialog();
+        }
     }
 }
