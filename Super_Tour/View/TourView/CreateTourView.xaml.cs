ddd7f234d04d52079d537ddf8fc943745a1d0a42
tuan <thanhtuanse16@gmail.com>
2023-05-17 20:28:53 +0700

    update Create Tour View

--- Super_Tour/View/TourView/CreateTourView.xaml.cs
+++ Super_Tour/View/TourView/CreateTourView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Super_Tour.ViewModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Super_Tour.View
@@ -18,42 +18,11 @@ namespace Super_Tour.View
     /// <summary>
     /// Interaction logic for CreateTourView.xaml
     /// </summary>
-    public partial class CreateTourView : Window
+    public partial class CreateTourView : UserControl
     {
         public CreateTourView()
         {
             InitializeComponent();
-            this.DataContext = new CreateTourViewModel();
-        }
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                this.DragMove();
-            }
-        }
-        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            DragMove();
-        }
-        private void btnClose_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-        private void btnMinimize_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-        private void btnMaximize_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.WindowState == WindowState.Normal)
-            {
-                this.WindowState = WindowState.Maximized;
-            }
-            else
-            {
-                this.WindowState = WindowState.Normal;
-            }
         }
     }
 }
