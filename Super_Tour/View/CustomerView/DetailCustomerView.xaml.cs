4e399d265793e58889475a5f20d44b81b2459b2d
tuan <thanhtuanse16@gmail.com>
2023-06-10 17:35:48 +0700

    add control event

--- Super_Tour/View/CustomerView/DetailCustomerView.xaml.cs
+++ Super_Tour/View/CustomerView/DetailCustomerView.xaml.cs
@@ -23,5 +23,24 @@ namespace Super_Tour.View
         {
             InitializeComponent();
         }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
     }
 }
